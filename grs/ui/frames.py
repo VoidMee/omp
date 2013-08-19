906e9e9fb7176c76ea74adf9b29519c61e975001
VoidMee <void.likeme@gmail.com>
2013-08-19 08:30:20 +0545

    Added label to upload image

--- grs/ui/frames.py
+++ grs/ui/frames.py
@@ -18,22 +18,27 @@ class RootFrame(wx.Frame):
         wx.Frame.__init__(self, None, -1, ROOTFRAMENAME, pos=(10,10), size=(1000, 650), style=wx.MINIMIZE_BOX|wx.SYSTEM_MENU|wx.CAPTION|wx.CLOSE_BOX|wx.CLIP_CHILDREN)
 
         #panel to hold controls for testing the project
-        self.testPanel = wx.Panel(self, pos=(300, 0), size=(700, 500), style = wx.TAB_TRAVERSAL | wx.BORDER_SIMPLE)
+        self.trainPanel = wx.Panel(self, pos=(300, 0), size=(700, 500), style = wx.TAB_TRAVERSAL | wx.BORDER_SIMPLE)
 
         #runButton to run the Project in Development Mode
-        self.runButton = wx.Button(self.testPanel, label="Start Project", pos=(500, 15), size=(150,30))
+        self.runButton = wx.Button(self.trainPanel, label="Start Training", pos=(500, 15), size=(150,30))
         #self.runButton = wx.Button(self, label="Start Project", pos=(700, 15), size=(160,30))
         self.Bind(wx.EVT_BUTTON, self._onOpen, self.runButton)
         #self.runButton.SetDefault();
 
+        self.fileUploadLabel = wx.StaticText(parent=self.trainPanel, id=-1, label="Upload Image Data to train:", pos=(50, 70))
+        self.fileUploadButton = wx.Button(parent=self.trainPanel, label="Browse", pos=(220, 60), size=(80, 30))
+        self.Bind(wx.EVT_BUTTON, self._onOpen, self.fileUploadButton)
+        #self.fileUploadDialog = wx.FileDialog(parent=self.testPanel, message="Choose a file", defaultDir="", defaultFile="", wildcard="*.*", style=0)
+
         #settingButton for setting the project
-        self.settingButton = wx.Button(self.testPanel, label="Settings", pos=(50,15), size=(60,30))
+        self.settingButton = wx.Button(self.trainPanel, label="Settings", pos=(50,15), size=(60,30))
 
         #panel to hold controls fot training the classifier
-        self.trainPanel = wx.Panel(self, name="Train Classifier", pos=(0,0), size=(300,500), style = wx.TAB_TRAVERSAL | wx.BORDER_SIMPLE)
+        self.testPanel = wx.Panel(self, name="Train Classifier", pos=(0,0), size=(300,500), style = wx.TAB_TRAVERSAL | wx.BORDER_SIMPLE)
 
         #trainButton for starting the training
-        self.trainButton = wx.Button(self.trainPanel, label="Star Training", pos=(50, 15), size=(160, 30))
+        self.trainButton = wx.Button(self.testPanel, label="Star Project", pos=(50, 15), size=(160, 30))
         #self.trainButton = wx.Button(self, label="Star Training", pos=(50, 15), size=(160, 30))
 
         #panel to show system event outputs
@@ -42,7 +47,8 @@ class RootFrame(wx.Frame):
         #sysEventLabel for displaying output events
         self.sysEventLabel = wx.StaticText(parent=self.sysEventPanel, id=-1, label="Output:", pos=(10,10))
 
-        self.fileUploadDialog = wx.FileDialog(parent=self.testPanel, message="Choose a file", defaultDir="", defaultFile="", wildcard="*.*", style=0)
+
+
 
     def _onStartProject(self, event):
         """
@@ -71,7 +77,7 @@ class RootFrame(wx.Frame):
         #"""
 
     def _onOpen(self, event):
-        saveFileDialog = wx.FileDialog(self, "Save XYZ file", "", "", "XYZ files (*.xyz)|*.xyz", wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
+        saveFileDialog = wx.FileDialog(self, "Save XYZ file", "", "", "Image files (*.jpg)|*.jpg", wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
         if saveFileDialog.ShowModal() == wx.ID_CANCEL:
             return     # the user changed idea...
         output_stream = wx.FileOutputStream(saveFileDialog.GetPath())
